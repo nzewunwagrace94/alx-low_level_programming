54a98dfe26b4673b46f00f9141ceaba00a00c5b9
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-06 17:34:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@ int main(void)
 {
 	/**
 	 * the main function is the entry point for C program
-	 * it print "Programming is like building a multilingual puzzle"
+	 * it print "Programming is like building a multilingual puzzle
 	 *
 	 * return 0 for successful execution
 	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle: \"");
 	return (0);
 }
