02449f4da34e8126138b8e440238b7a87b259bf3
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-07 06:53:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	printf("size of int: %zu 4 bytes\n", sizeof(int));
-	printf("size of char: %zu 1 byte\n", sizeof(char));
-	printf("Size of float: %zu 4 bytes\n", sizeof(float));
-	printf("size of double: %zu 4 bytes\n", sizeof(double));
+	printf("size of int: %zu bytes\n", sizeof(int));
+	printf("size of char: %zu byte\n", sizeof(char));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("size of double: %zu bytes\n", sizeof(double));
 	return (0);
 }
