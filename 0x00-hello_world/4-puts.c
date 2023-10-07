457a85e8ce7e811eb8ba0ebe579de55d6f54cd38
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-07 03:49:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-	/**
-	 * the main function is the entry point for C program
-	 * it print "Programming is like building a multilingual puzzle
-	 *
-	 * return 0 for successful execution
-	 */
-	puts("Programming is like building a multilingual puzzle: \"");
+	puts("\"Programming is like building a multilingual puzzl");
 	return (0);
 }
