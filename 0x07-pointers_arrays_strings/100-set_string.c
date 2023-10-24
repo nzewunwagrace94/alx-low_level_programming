06ba6b37e493174b1959b0a9082b1c06836a0fb6
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-25 00:05:58 +0100

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - it sets the pointer to a char or string
+ * @s: the pointer to pointer
+ * @to: pointer to char
+ * Return: always 0 as success
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
