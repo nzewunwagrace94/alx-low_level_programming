1fdacc4fc61d8f503a8d739b0b805a24ec707d2e
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-26 00:47:01 +0100

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,16 +4,14 @@
 /**
  * _print_rev_recursion - function that prints a string in reverse
  * @s: string to be printed
- * Return: always 0
+ * Return: always 0 as successful
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
+	}
 }
