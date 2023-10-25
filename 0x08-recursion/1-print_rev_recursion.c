dbc3b15d6249fbab77420410516674d48fba2df9
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-25 22:50:43 +0100

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,8 +11,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	 	_print_rev_recursion(++s);
- 		 _putchar(*s);	 
+		_print_rev_recursion(++s);
+		_putchar(*s);
 	}
 	else
 		_putchar('\n');
