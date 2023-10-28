d18f150596b522ad72204e151e77d50092959c96
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-28 23:37:52 +0100

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,10 +4,27 @@
  * main - program that multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 successful
+ * @atoi: convert string to an integer
+ * Return: 0 (Successfu), 1 (Error)
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+	int x, y;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argc[1]);
+	y = atoi(argc[2]);
+
+	result = x * y;
+
+	printf("%d\n", x * y);
+
+	return (0);
 }
+
