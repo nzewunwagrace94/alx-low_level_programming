a3663d48068e5392a50de76338b2b3393722ccff
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-10-28 22:14:23 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 successful
+ */
+
+int main(int argc, char *argv)
+{
+
+}
