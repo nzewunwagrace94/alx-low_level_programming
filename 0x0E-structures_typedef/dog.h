4eef7b230c1a5fe4b0166e26b245c07d45c30b7e
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-11-07 22:54:49 +0100

    0-dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure
+ * @name: name of do
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
