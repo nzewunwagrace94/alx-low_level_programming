0b05d3a829712c00854b77696a5e3a3c66ae6985
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-11-10 00:41:38 +0100

    objdump -d -j.text -M intel main

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+  2 #include <stdio.h>
+  3 #include <stdlib.h>
+  4
+  5 /**
+  6  * op_ - sum two numbers
+  7  * @a: first number
+  8  * @b: second number
+  9  * Return: sum of two numbers
+ 10  */
+ 11 int op_add(int a, int b)
+ 12 {
+ 13         return (a + b);
+ 14 }
+ 15
+ 16 /**
+ 17  * op_sub - substract two numbers
+ 18  * @a: first number
+ 19  * @b: second number
+ 20  * Return: difference of a and b
+ 21  */
+ 22
+ 23 int op_sub(int a, int b)
+ 24 {
+ 25         return (a - b);
+ 26 }
+ 27
+ 28 /**
+ 29  * op_mul - multiply two numbers
+ 30  * @a: first number
+  * @b: second number
+  * Return: product of a and b
+  */
+ int op_mul(int a, int b)
+ {
+   
