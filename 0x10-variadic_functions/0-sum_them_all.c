f2b03731fa63db959f1c95fead1648c7d3f90fb7
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-11-10 16:29:39 +0100

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
 	unsigned int i;
+	int sum = 0;
 	va_list args;
 
 	if (n == 0)
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, const unsigned int);
+		sum += va_arg(args, int);
 	}
 	va_end(args);
 	return (sum);
