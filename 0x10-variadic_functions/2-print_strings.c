2f33fd2066fd5f4e25395d6514e7352177a4349f
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2023-11-10 18:17:40 +0100

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings
+ * @n: number of arguments
+ * @separator: string to be printed between strings
+ *
+ * Return: 0 success
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list ptr_str;
+
+	va_start(ptr_str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ptr_str, char *);
+
+		if (str == NULL)
+		{
+			printf("(nill)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr_str);
+}
