e7173e1d5c51d3be0d4ad501b100a1ad18607645
nzewunwagrace94 <nzewunwagrace94>
2023-11-17 16:18:05 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void ComeFirst(void)__attribute__ ((constructor));
+/**
+ * ComeFirst - fuction that print sentence
+ */
+
+void ComeFirt(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
