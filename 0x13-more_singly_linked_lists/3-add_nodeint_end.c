252c4d35389a5796d7b0fc476d08729863d7a4b6
nzewunwagrace94 <nzewunwagrace94>
2023-11-20 18:52:00 +0100

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,13 @@
+#include "list.h"
+
+/**
+ * add_nodeint_end -  adds a new node at the end of a listint_t
+ * @head: pointer to the head
+ * @n: number of element to add
+ * Return:  the address of the new element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t 
+}
