da1702990e018cd5a2c0fa5a6e243656cf1a7e3a
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-02-01 21:17:09 -0800

    hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table.
+ * @size: is the size of the array
+ * Returns: NULL if an error occurs and a pointer to the created hash table ht
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
